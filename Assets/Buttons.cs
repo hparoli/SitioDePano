d0e3289aca4026f01a3c06f5b13895de261ff280
Henrique Paroli <henrique.paroli@gmail.com>
2018-02-21 01:33:23 -0300

    Genius
    Genius básico feito

---
+++ Assets/Buttons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+    public int idButton;
+
+    public GameController gameController;
+
+
+    private void OnMouseDown()
+    {
+        if(gameController.gameState == GameState.RESPONDER)
+            gameController.StartCoroutine("Responder", idButton);
+    }
+}
