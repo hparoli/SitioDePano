a0116c2e5be81591664315f854e35787793466a4
Indianoga <36790447+Indianoga@users.noreply.github.com>
2018-03-20 11:25:39 -0300

    Pique - esconde
    Foi adicionado o game de pique esconde

---
+++ Assets/03_Scripts/DollSelect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DollSelect : MonoBehaviour {
+
+	private int idTema;
+
+	private float acertos;
+	private float notaFinal;
+	private float media;
+
+
+	void Start(){
+		idTema = PlayerPrefs.GetInt ("idTema");
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Mouse is down");
+
+			RaycastHit hitInfo = new RaycastHit();
+			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+			if (hit) 
+			{
+				Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+				if (hitInfo.transform.gameObject.tag == "Doll") 
+				{
+					acertos = 10;
+				}
+
+			}
+
+			ToScore ();
+		}
+	}
+
+	void ToScore(){
+
+
+
+		SceneManager.LoadScene ("Score");
+	}
+}
---
+++ Assets/03_Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	//B: Random Spawn Point toda vez que rodar o jogo
+
+	public GameObject dollObj;                // prefab da boneca
+	public Transform[] spawnPoints;         // Array c/ Spawn Points
+
+
+
+	void Start () {
+	
+		Invoke ("Spawner", 0);
+	}
+
+	void Spawner() {
+	
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		Instantiate (dollObj, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		Debug.Log ("capeta");
+
+	}
+}
