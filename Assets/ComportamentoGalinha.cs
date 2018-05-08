f5e5b96cfbe7ba137dd3bea3963194a8f409398f
Henrique Paroli <henrique.paroli@gmail.com>
2018-05-08 00:18:01 -0300

    Coletando Ovos

---
+++ Assets/ComportamentoGalinha.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class ComportamentoGalinha : MonoBehaviour {
+
+	public Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = this.gameObject.GetComponent<Animator>();
+		StartCoroutine("Comportamento");
+	}
+	
+	IEnumerator Comportamento(){
+		int tempo = Random.Range(5, 16);
+		yield return new WaitForSeconds(tempo);
+		animator.SetBool("Levantando",true);
+		yield return new WaitForSeconds(2.5f);
+		animator.SetBool("Sentando",true);
+		animator.SetBool("Levantando",false);
+		yield return new WaitForSeconds(0.5f);
+		animator.SetBool("Sentando",false);
+	}
+}
