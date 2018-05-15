fd6bb1587ecea99b98a407f09bcfb55457d49d87
Henrique Paroli <henrique.paroli@gmail.com>
2018-05-14 23:33:36 -0300

    De Olho No Lobo
    90% feito

---
+++ Assets/01_Scripts/DeOlhoNoLobo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeOlhoNoLobo : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] spawns;
+
+	[SerializeField]
+	private GameObject lobo;
+
+	[SerializeField]
+	private float delay, time;
+
+	public int ovelhas;
+	// Use this for initialization
+	void Start () {
+		time = 0f;
+		delay = 3f;
+		ovelhas = 0;
+		StartCoroutine("SpawnLobo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(time < 45)
+			time += Time.deltaTime;
+
+		if(time >= 20)
+			delay = 1.5f;
+		
+		if(time >= 30)
+			delay = 1f;
+		
+		if(time >= 40)
+			delay = 0.5f;
+		
+		if(ovelhas == 10)
+			StartCoroutine("GameOver");
+	}
+
+	IEnumerator SpawnLobo(){
+		int spawnPointIndex = Random.Range (0, spawns.Length);
+		Instantiate (lobo, spawns[spawnPointIndex].transform.position, spawns[spawnPointIndex].transform.rotation);
+		yield return new WaitForSeconds(delay);
+		if(time < 45)
+			StartCoroutine("SpawnLobo");
+		else
+			StartCoroutine("GameOver");
+	}
+
+	IEnumerator GameOver(){
+		yield return new WaitForSeconds(2f);
+		SceneManager.LoadScene(7);
+	}
+
+	public void SetOvelhas(){
+		ovelhas++;
+	}
+}
---
+++ Assets/01_Scripts/LoboBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoboBehavior : MonoBehaviour {
+
+	[SerializeField]
+	private int delaySpawn, waypointIndex;
+
+	[SerializeField]
+	private GameObject[] waypoints;
+
+	[SerializeField]
+	private float moveSpeed;
+	
+	public GameObject gm;
+
+	[SerializeField]
+	private bool move;
+
+	// Use this for initialization
+	void Start () {
+		gm = GameObject.Find("GameManager");
+		delaySpawn = 3;
+		moveSpeed = 2.5f;
+		waypoints = new GameObject[5];
+		move = false;
+		waypoints[0] = GameObject.Find("waypoint1");
+		waypoints[1] = GameObject.Find("waypoint2");
+		waypoints[2] = GameObject.Find("waypoint3");
+		waypoints[3] = GameObject.Find("waypoint4");
+		waypoints[4] = GameObject.Find("waypoint5");
+		
+		this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+		StartCoroutine("Walk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(move)
+			Move();
+
+		RaycastHit click = new RaycastHit();
+		bool hit = Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out click);
+		if (Input.GetMouseButtonDown (0)) {
+			if(hit){
+				if(click.transform.gameObject.tag == "lobo"){
+					Destroy(this.gameObject);
+					//feedback pegando lobo
+				}
+			}	
+		}
+	}
+
+	
+	IEnumerator Walk(){
+		yield return new WaitForSeconds(delaySpawn);
+		this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+		move = true;
+	}
+
+	public void Move(){
+		this.transform.position = Vector2.MoveTowards(this.transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.tag == "waypoints"){
+			gm.GetComponent<DeOlhoNoLobo>().SetOvelhas();
+			//feedback lobo pegando ovelha
+			Destroy(this.gameObject, 1);
+		}
+	}
+}
