87d1713852cad68b63e2943c9cfca278b9265ce8
Henrique Paroli <henrique.paroli@gmail.com>
2018-05-20 21:04:06 -0300

    Contando Bixos
    Comecei a montar o Contando Bixos e Arrumei algumas coisas no De Olho no Lobo

--- Assets/01_Scripts/DeOlhoNoLobo.cs
+++ Assets/01_Scripts/DeOlhoNoLobo.cs
@@ -2,40 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DeOlhoNoLobo : MonoBehaviour {
 
 	[SerializeField]
-	private GameObject[] spawns;
+	private GameObject[] spawns, arbustos;
 
 	[SerializeField]
 	private GameObject lobo;
 
 	[SerializeField]
-	private float delay, time;
+	private float delay, time, moveSpeed;
+
+	[SerializeField]
+	private Text cronometro;
 
 	public int ovelhas;
 	// Use this for initialization
 	void Start () {
-		time = 0f;
+		time = 45f;
 		delay = 3f;
 		ovelhas = 0;
+		moveSpeed = 2.5f;
 		StartCoroutine("SpawnLobo");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(time < 45)
-			time += Time.deltaTime;
+		if(time > 0){
+			time -= Time.deltaTime;
+			cronometro.text = time.ToString("f0");
+		} else {
+			time = 0;
+		}
 
-		if(time >= 20)
+		if(time <= 30){
 			delay = 2.5f;
+			moveSpeed = 3f;
+		}
 		
-		if(time >= 30)
+		if(time <= 20){
 			delay = 2f;
+			moveSpeed = 3.5f;
+		}
 		
-		if(time >= 40)
+		if(time <= 10){
 			delay = 1.5f;
+			moveSpeed = 4f;
+		}
+
 		
 		if(ovelhas == 10)
 			StartCoroutine("GameOver");
@@ -43,12 +59,23 @@ public class DeOlhoNoLobo : MonoBehaviour {
 
 	IEnumerator SpawnLobo(){
 		int spawnPointIndex = Random.Range (0, spawns.Length);
-		Instantiate (lobo, spawns[spawnPointIndex].transform.position, spawns[spawnPointIndex].transform.rotation);
+		GameObject loboGO = Instantiate (lobo, spawns[spawnPointIndex].transform.position, spawns[spawnPointIndex].transform.rotation);
+		StartCoroutine("Arbusto", spawnPointIndex);
+		yield return new WaitForSeconds(0.1f);
+		loboGO.GetComponent<LoboBehavior>().SetWaypointIndex(spawnPointIndex, moveSpeed);
 		yield return new WaitForSeconds(delay);
-		if(time < 45)
+		
+		if(time > 0){
 			StartCoroutine("SpawnLobo");
-		else
+		} else{
 			StartCoroutine("GameOver");
+		}
+	}
+
+	IEnumerator Arbusto(int index){
+		arbustos[index].GetComponent<Animator>().SetBool("Mexe",true);
+		yield return new WaitForSeconds(2f);
+		arbustos[index].GetComponent<Animator>().SetBool("Mexe",false);
 	}
 
 	IEnumerator GameOver(){
