4610475c7b713b22f729b707e35fbef7bf4f11bd
MarceloGD <36715844+MarceloGD@users.noreply.github.com>
2018-06-04 23:42:47 -0300

    Arte Tutorial De Olho nos Lobos

--- Assets/01_Scripts/DeOlhoNoLobo.cs
+++ Assets/01_Scripts/DeOlhoNoLobo.cs
@@ -44,36 +44,26 @@ public class DeOlhoNoLobo : MonoBehaviour {
 		tutorial.SetActive (true);
 		infoTutorial.text = txtTutorial [indexTutorial];
 		boardsTutorial [0].SetActive (true);boardsTutorial [1].SetActive (false);boardsTutorial [2].SetActive (false);
-		imagesTutorial [0].SetActive (true);imagesTutorial [1].SetActive (false);imagesTutorial [2].SetActive (false);
+		imagesTutorial [0].SetActive (true);imagesTutorial [1].SetActive (false);
 	}
 
 	public void ChangeTextTutorialForward(){
 		indexTutorial++;
 		infoTutorial.text = txtTutorial [indexTutorial];
+
 		if(indexTutorial == 0){
 			boardsTutorial [0].SetActive (true);boardsTutorial [1].SetActive (false);boardsTutorial [2].SetActive (false);
 			imagesTutorial [0].SetActive (true);
 			imagesTutorial [1].SetActive (false);
-			imagesTutorial [2].SetActive (false);
+	
 		}
 		if(indexTutorial == 1){
-			boardsTutorial [0].SetActive (true);boardsTutorial [1].SetActive (true);boardsTutorial [2].SetActive (false);
+			boardsTutorial [0].SetActive (false);boardsTutorial [1].SetActive (true);boardsTutorial [2].SetActive (true);
 			imagesTutorial [0].SetActive (false);
 			imagesTutorial [1].SetActive (true);
-			imagesTutorial [2].SetActive (false);
+
 		}
-		if(indexTutorial == 2){
-			boardsTutorial [0].SetActive (true);boardsTutorial [1].SetActive (true);boardsTutorial [2].SetActive (false);
-			imagesTutorial [0].SetActive (false);
-			imagesTutorial [1].SetActive (false);
-			imagesTutorial [2].SetActive (true);
-		}
-		if(indexTutorial == 3){
-			boardsTutorial [0].SetActive (false);boardsTutorial [1].SetActive (true);boardsTutorial [2].SetActive (true);
-			imagesTutorial [0].SetActive (true);
-			imagesTutorial [1].SetActive (false);
-			imagesTutorial [2].SetActive (false);
-		}
+
 	}
 
 	public void ChangeTextTutorialBack(){
@@ -83,25 +73,14 @@ public class DeOlhoNoLobo : MonoBehaviour {
 			boardsTutorial [0].SetActive (true);boardsTutorial [1].SetActive (false);boardsTutorial [2].SetActive (false);
 			imagesTutorial [0].SetActive (true);
 			imagesTutorial [1].SetActive (false);
-			imagesTutorial [2].SetActive (false);
+
 		}
+
 		if(indexTutorial == 1){
-			boardsTutorial [0].SetActive (true);boardsTutorial [1].SetActive (true);boardsTutorial [2].SetActive (false);
+			boardsTutorial [0].SetActive (false);boardsTutorial [1].SetActive (true);boardsTutorial [2].SetActive (true);
 			imagesTutorial [0].SetActive (false);
 			imagesTutorial [1].SetActive (true);
-			imagesTutorial [2].SetActive (false);
-		}
-		if(indexTutorial == 2){
-			boardsTutorial [0].SetActive (true);boardsTutorial [1].SetActive (true);boardsTutorial [2].SetActive (false);
-			imagesTutorial [0].SetActive (false);
-			imagesTutorial [1].SetActive (false);
-			imagesTutorial [2].SetActive (true);
-		}
-		if(indexTutorial == 3){
-			boardsTutorial [0].SetActive (false);boardsTutorial [1].SetActive (true);boardsTutorial [2].SetActive (true);
-			imagesTutorial [0].SetActive (true);
-			imagesTutorial [1].SetActive (false);
-			imagesTutorial [2].SetActive (false);
+		
 		}
 
 	}
