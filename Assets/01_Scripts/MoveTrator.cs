2addb843709cb271df33700d86a0aec911384fe2
MarceloGD <36715844+MarceloGD@users.noreply.github.com>
2018-06-10 18:58:31 -0300

    Animação Loading

---
+++ Assets/01_Scripts/MoveTrator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrator : MonoBehaviour {
+
+	public float velX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (new Vector2 (transform.position.x * velX * Time.deltaTime , 0));
+	}
+}
