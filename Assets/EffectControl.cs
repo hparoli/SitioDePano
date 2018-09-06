fe80941aafc3840c2d36f0225fff9bf8c5d6ef32
Indianoga <36790447+Indianoga@users.noreply.github.com>
2018-09-06 19:11:38 -0300

    AddEffect

---
+++ Assets/EffectControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectControl : MonoBehaviour {
+
+	public GameObject touchEffect;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		AddEffect ();
+	}
+	private void AddEffect ()
+	{
+		Vector3 mousPos =  Input.mousePosition;
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Debug.Log(mousPos);
+			Instantiate(touchEffect, mousPos, transform.rotation);
+
+		}
+	}
+}
