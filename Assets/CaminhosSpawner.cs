101b625414a706841df79c8ad33b144ea0f7e3ff
Henrique Paroli <henrique.paroli@gmail.com>
2018-09-21 22:28:23 -0300

    Caminho
    começo

--- Assets/CaminhosSpawner.cs
+++ Assets/CaminhosSpawner.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class CaminhosSpawner : MonoBehaviour {
 
-	private int numColunas,numLinhas,level;
+	[SerializeField]
+	private int numColunas,numLinhas,level,resp;
 
+	[SerializeField]
 	private GameObject seta;
 
+	[SerializeField]
+	private List<GameObject> setas;
+
+	[SerializeField]
+	private Transform pos;
 
 	void Start(){
 		level = 1;
+		InicializaLevel(level);
 	}
 
 	private void InicializaLevel(int lvl){
 		if (lvl == 1 || lvl == 2 || lvl == 3 || lvl == 4){
 			numColunas = 4;
 			numLinhas = 4;
+			resp = 1;
 		}
 		else if (lvl == 5){
 			numColunas = 4;
@@ -33,6 +42,20 @@ public class CaminhosSpawner : MonoBehaviour {
 		else if (lvl == 8){
 			numColunas = 4;
 			numLinhas = 8;
+		}
+
+		int index = 0;
+		for (int i = 0; i <= numColunas; i++)
+		{
+			for (int j = 0; j < numLinhas; j++)
+			{
+				setas.Add(GameObject.Instantiate(seta, new Vector3(pos.position.x - (i*2), pos.position.y - (j*1.5f), pos.position.z) ,pos.rotation));
+				setas[index].GetComponent<SetaBehavior>().x = j;
+				setas[index].GetComponent<SetaBehavior>().y = i;
+				setas[index].GetComponent<SetaBehavior>().dir = 0;
+				setas[index].GetComponent<SetaBehavior>().cor = Color.white;
+				setas[index].GetComponent<SetaBehavior>().tipoSeta = "normal";
+			}
 		}
 	}
 }
---
+++ Assets/SetaBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetaBehavior : MonoBehaviour{
+
+	public int x, y, dir;
+
+	public string tipoSeta;
+
+	public Color cor;
+
+}
