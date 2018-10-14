d2bad0c8770fb74a9023795681805644d1bd2e0c
Henrique Paroli <henrique.paroli@gmail.com>
2018-10-14 20:11:09 -0300

    Horta da Aninha
    Segue o jogo. Se quiser testar todas as fases só trocar no script a variável level para 1, 2 ou 3.

--- Assets/HortaController.cs
+++ Assets/HortaController.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class HortaController : MonoBehaviour {
 
-	private int level;
+	public int level;
+	
+	public float saldoInicial,saldo;
 
 	[SerializeField]
 	private GameObject[] buttons;
@@ -14,34 +18,146 @@ public class HortaController : MonoBehaviour {
 	private Text[] texts;
 
 	[SerializeField]
-	private Text saldo;
+	private Sprite[] imagens;
 
+	[SerializeField]
+	private Image[] legumes;
+
+	[SerializeField]
+	private Text saldoTxt,mensagem;
+
+	private bool podeJogar,gameover;
 
 	// Use this for initialization
 	void Start () {
 		level = 1;
-
+		podeJogar = true;
+		gameover = false;
 		StartGame();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(saldo < 0 || legumes[5].GetComponent<LegumesControl>().plantou){
+			podeJogar = false;
+			if(!gameover) StartCoroutine("GameOver");
+		}
 	}
 
 	void StartGame(){
-		if(level == 1){
+		if(level == 1 || level == 3){
 			for (int i = 0; i < buttons.Length; i++)
 			{
 				if(i < 3) buttons[i].SetActive(true);
 			}
 		}
-		else if(level > 1){
+		else if(level == 2){
 			for (int i = 0; i < buttons.Length; i++)
 			{
 				buttons[i].SetActive(true);
 			}
 		}
 		
+		if(level == 1){
+			buttons[0].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem11",9);});
+			buttons[0].GetComponentInChildren<Text>().text = "R$9,00";
+			buttons[1].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem12",7);});
+			buttons[1].GetComponentInChildren<Text>().text = "R$7,00";
+			buttons[2].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem13",4);});
+			buttons[2].GetComponentInChildren<Text>().text = "R$4,00";
+			saldoInicial = saldo = 37;
+			saldoTxt.text = "R" + saldoInicial.ToString("C2");
+			saldoTxt.text = saldoTxt.text.Replace(".",",");
+		} else if (level == 2){
+			buttons[0].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem21",2);});
+			buttons[0].GetComponentInChildren<Text>().text = "R$2,00";
+			buttons[1].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem22",3);});
+			buttons[1].GetComponentInChildren<Text>().text = "R$3,00";
+			buttons[2].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem23",7);});
+			buttons[2].GetComponentInChildren<Text>().text = "R$7,00";
+			buttons[3].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem24",10);});
+			buttons[3].GetComponentInChildren<Text>().text = "R$10,00";
+			saldoInicial = saldo = 31;
+			saldoTxt.text = "R" + saldoInicial.ToString("C2");
+			saldoTxt.text = saldoTxt.text.Replace(".",",");
+		} else if (level == 3){
+			buttons[0].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem21",2.25f);});
+			buttons[0].GetComponentInChildren<Text>().text = "R$2,25";
+			buttons[1].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem22",3.25f);});
+			buttons[1].GetComponentInChildren<Text>().text = "R$3,25";
+			buttons[2].GetComponent<Button>().onClick.AddListener(delegate{CompraSemente("sem23",1.75f);});
+			buttons[2].GetComponentInChildren<Text>().text = "R$1,75";
+			saldoInicial = saldo = 14.50f;
+			saldoTxt.text = "R" + saldoInicial.ToString("C2");
+			saldoTxt.text = saldoTxt.text.Replace(".",",");
+		}
+	}
+
+	public void CompraSemente(string sem, float vlr){
+		if(podeJogar){
+		for (int i = 0; i < legumes.Length; i++)
+		{
+			if(!legumes[i].GetComponent<LegumesControl>().plantou){
+				if(sem == "sem11" || sem == "sem21" || sem == "sem31"){
+					legumes[i].sprite = imagens[0];
+				} else if(sem == "sem12" || sem == "sem22" || sem == "sem32"){
+					legumes[i].sprite = imagens[1];
+				} else if(sem == "sem13" || sem == "sem23" || sem == "sem33"){
+					legumes[i].sprite = imagens[2];
+				} else if(sem == "sem24" || sem == "sem34"){
+					legumes[i].sprite = imagens[3];
+				} 
+				legumes[i].GetComponent<LegumesControl>().plantou = true;
+				saldo -= vlr;
+				saldoTxt.text = "R$" + saldo.ToString("C2");
+				saldoTxt.text = saldoTxt.text.Replace(".",",");
+				break;
+			}
+		}
+		}
+	}
+
+	public void Resetar(){
+		for (int i = 0; i < legumes.Length; i++)
+		{
+			legumes[i].sprite = null;
+			legumes[i].GetComponent<LegumesControl>().plantou = false;
+		}
+		saldo = saldoInicial;
+		saldoTxt.text = "R$" + saldo.ToString("C2");
+		saldoTxt.text = saldoTxt.text.Replace(".",",");
+		gameover = false;
+		podeJogar = true;
+	}
+
+	IEnumerator GameOver(){
+		gameover = true;
+		if (saldo != 0){
+			//som de erro
+			yield return new WaitForSeconds(2f);
+			Resetar();
+			StopCoroutine("GameOver");
+		} else if (saldo == 0 && legumes[5].GetComponent<LegumesControl>().plantou){
+			mensagem.text = "Parabéns! Você acertou!";
+			for (float f = 0f; f <= 1; f += 0.02f){
+                Color c = mensagem.color;
+		    	c.a = f;
+		    	mensagem.color = c;
+		    	new WaitForSeconds(.5f);
+                yield return null;
+			}
+			
+			yield return new WaitForSeconds(1f);
+
+			for (float f = 1f; f > 0; f -= 0.02f){
+					Color c = mensagem.color;
+					c.a = f;
+					mensagem.color = c;
+					new WaitForSeconds(.5f);
+					yield return null;
+			}
+
+			SceneManager.LoadScene("Score");
+		}
 	}
 }
---
+++ Assets/LegumesControl.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegumesControl : MonoBehaviour{
+	public bool plantou = false;
+
+}
