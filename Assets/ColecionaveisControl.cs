facdeb9a0a98c6ddcfdb7bc342eb954d8e5afadb
Indianoga <36790447+Indianoga@users.noreply.github.com>
2018-11-13 18:14:56 -0200

    Colecionavies

---
+++ Assets/ColecionaveisControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColecionaveisControl : MonoBehaviour {
+
+	[SerializeField]
+	GameObject colect;
+
+	int value;
+	int teste = 20;
+	// Use this for initialization
+	
+	void Start ()
+	{
+		PlayerPrefs.SetInt("piqueCollect",teste);
+		value =	PlayerPrefs.GetInt("piqueCollect");
+		
+		Debug.Log(value);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		OpenItens();
+		
+	}
+
+	public void OpenItens()
+	{
+	
+		if (value >= 20)
+		{
+			colect.SetActive(true);
+		}
+	}
+}
